81e80e4010536247777874a90470d486cd6700f8
lsyurievna <lsyurievna@mun.ca>
2021-07-22 00:16:51 -0230

    Three html pages create, still witout any appreciable content

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return render_template("hello.html")
+def about():
+  return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+  return render_template("projects.html")
+
+@app.route("/contacts")
+def contacts():
+  return render_template("contacts.html")
