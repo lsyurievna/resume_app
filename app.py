43777dd338d5208dd7d9ca891992795915d0fb3a
lsyurievna <lsyurievna@mun.ca>
2021-07-22 17:56:13 -0230

    Some conditional logic tests implemented, pretty cool

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+name = "Mila"
+list_of_stuff = ["Blanket", "Pillow", "Puter"]
 
 @app.route("/")
 def about():
-  return render_template("about.html")
+  return render_template("about.html", name=name,
+  list_of_stuff = list_of_stuff)
 
 @app.route("/projects")
 def projects():
-  return render_template("projects.html")
+  return render_template("projects.html", name=name)
 
 @app.route("/contacts")
 def contacts():
-  return render_template("contacts.html")
+  return render_template("contacts.html", name = name)
