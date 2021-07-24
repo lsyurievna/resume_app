5d1f9415af4c4243ebfcccaa31d2b2420013afef
lsyurievna <lsyurievna@mun.ca>
2021-07-24 11:25:42 -0230

    The contact form introduced, a problem of sending emails via Formspree was incountered howe

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template
+import decouple 
 
 app = Flask(__name__)
+
 name = "Mila"
-list_of_stuff = ["Blanket", "Pillow", "Puter"]
+contact = decouple.config("CONTACT_FORM_API", default=None)
 
 @app.route("/")
 def about():
-  return render_template("about.html", name=name,
-  list_of_stuff = list_of_stuff)
+  return render_template("about.html", name=name)
 
-@app.route("/projects")
-def projects():
-  return render_template("projects.html", name=name)
+@app.route("/CV")
+def CV():
+  return render_template("CV.html", name=name)
 
 @app.route("/contacts")
 def contacts():
-  return render_template("contacts.html", name = name)
+  return render_template("contacts.html", name = name, api = contact)
