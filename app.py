796e0b3e76aafea31410fb0f52902da65d230ba8
lsyurievna <lsyurievna@mun.ca>
2021-07-27 20:24:59 -0230

    Links to posts added

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 import decouple 
-import requests
+import os
+from pathlib import Path
 
 app = Flask(__name__)
 
 name = "Mila"
 contact = decouple.config("CONTACT_FORM_API" )
+blog_posts = []
+
+#If a file in the blog dir has ".md" extension
+#print its name
+with os.scandir("blog") as it:
+  for entry in it:
+    if entry.name.endswith(".md") and entry.is_file():
+      post_data = Path(entry.path).read_text()
+      
+      blog_posts.append({
+        "name": entry.name,
+        "data": post_data
+      })
+     
 
 @app.route("/")
 def about():
@@ -18,3 +33,11 @@ def CV():
 @app.route("/contacts")
 def contacts():
   return render_template("contacts.html", name = name, api = contact)
+
+@app.route("/blog_listing")
+def blog_listing():
+  return render_template("blog_listing.html", name = name, blog_posts = blog_posts)
+
+@app.route("/blog_listing/<post_name>")
+def blog_entry(post_name):
+  return render_template("blog_entry.html", name = name, post_name = post_name)
---
+++ blog/second_post.md
@@ -0,0 +1,4 @@
+#second post
+
+1. second possst
+2. my second post
